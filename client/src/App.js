5bd624f037cde6d99464ce4a5a8069dad26cd700
omarkhs <30681552+omarkhs@users.noreply.github.com>
2020-03-30 22:12:30 -0700

    update client deps, and connect react front end with express server

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,13 @@ class App extends Component {
   componentDidMount() {
     // Call our fetch function below once the component mounts
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+      .then((res) => this.setState({ data: res.message }))
+      .catch((err) => console.log(err));
   }
   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    const response = await fetch('/express');
     const body = await response.json();
-
     if (response.status !== 200) {
       throw Error(body.message);
     }
