77fc9c8c42691f5c5df6d981668b8ed38814b4d5
omarkhs <30681552+omarkhs@users.noreply.github.com>
2020-03-30 22:10:00 -0700

    add backed entry point

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import connectCitiesRoutes from "./routes/city.routes"
+import { json, urlencoded } from "body-parser";
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+// parse requests of content-type: application/json
+server.use(json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+server.use(urlencoded({ extended: true }));
+
+// GET endpoint route
+server.get("/express", (req, res) => {
+  res.json({ message: "EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
+
+server.get("/", (req, res) => {
+  res.json({ message: "Just Travelous Express server" });
+});
+
+// Connect server with routes created
+connectCitiesRoutes(server);
+
+// set port, listen for requests
+server.listen(port, () => console.log(`Listening on port ${port}`));
