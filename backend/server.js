70c8e734350348ccae5d554390a7de6bb33f60cd
omarkhs <30681552+omarkhs@users.noreply.github.com>
2020-04-01 08:03:55 -0700

    add cors to backend to allow cross origin requests

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
 import express from 'express';
-import connectCitiesRoutes from "./routes/city.routes"
-import { json, urlencoded } from "body-parser";
-
+import connectCitiesRoutes from './routes/city.routes';
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
 const server = express();
 const port = process.env.PORT || 5000;
 
 // parse requests of content-type: application/json
 server.use(json());
-
+server.use(cors());
 // parse requests of content-type: application/x-www-form-urlencoded
 server.use(urlencoded({ extended: true }));
 
 // GET endpoint route
-server.get("/express", (req, res) => {
-  res.json({ message: "EXPRESS BACKEND IS CONNECTED TO REACT" });
+server.get('/express', (req, res) => {
+  res.json({ message: 'EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-server.get("/", (req, res) => {
-  res.json({ message: "Just Travelous Express server" });
+server.get('/', (req, res) => {
+  res.json({ message: 'Just Travelous Express server' });
 });
 
 // Connect server with routes created
