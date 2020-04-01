a48776c2fb7449b04ffcd515f05216482f7fc291
omarkhs <30681552+omarkhs@users.noreply.github.com>
2020-04-01 09:28:19 -0700

    add front end based form validation

--- client/src/components/AddCityFrom.js
+++ client/src/components/AddCityFrom.js
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form';
 import { Button, TextField, Paper, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     ...theme.mixins.gutters(),
@@ -19,53 +18,75 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AddCityForm() {
   const classes = useStyles();
+  const { register, handleSubmit, errors, reset } = useForm();
+  const textFieldProps = {
+    type: 'text',
+    variant: 'outlined',
+    size: 'small',
+    fullWidth: true,
+    required: true,
+    margin: 'normal',
+  };
 
-  const { register, handleSubmit, errors } = useForm();
+  const cityNameValidation = {
+    required: {
+      value: true,
+      message: 'This field is required',
+    },
+    maxLength: {
+      value: 100,
+      message: "City name can't be more than 100 characters long",
+    },
+  };
+
+  const postalCodeValidation = {
+    required: {
+      value: true,
+      message: 'This field is required',
+    },
+    maxLength: {
+      value: 7,
+      message: "Postal code can't be more than 7 characters",
+    },
+    pattern: {
+      value: /[0-9][A-Z] ?[0-9][A-Z][0-9]/i,
+      message: 'Postal code should match the following format A1A 1A1',
+    },
+  };
+
   const onSubmit = (data) => {
     console.log(data);
     CityHttpService.create(data)
       .then((response) => {
+        // res data returns newly add city
         console.log('response of post request adding new city', response);
       })
       .catch((e) => {
         console.log('error in post request adding new city', e);
       });
+    reset({ city_name: '', post_code: '' });
   };
-
-  console.log('error due to', errors);
 
   return (
     <Container maxWidth='xs'>
       <Paper className={classes.root} elevation={3}>
         <form>
           <TextField
-            type='text'
-            placeholder='City Name'
             name='city_name'
-            variant='outlined'
-            size='small'
-            fullWidth={true}
-            inputRef={register({
-              required: true,
-              maxLength: 100,
-            })}
+            label='City Name'
+            {...textFieldProps}
+            error={errors.city_name ? true : false}
+            helperText={errors.city_name ? errors.city_name.message : ''}
+            inputRef={register(cityNameValidation)}
           />
-          <br />
-          <br />
           <TextField
-            type='text'
-            placeholder='Postal Code'
             name='post_code'
-            variant='outlined'
-            size='small'
-            fullWidth={true}
-            inputRef={register({
-              required: true,
-              maxLength: 7,
-            })}
+            label='Postal Code'
+            error={errors.post_code ? true : false}
+            helperText={errors.post_code ? errors.post_code.message : ''}
+            {...textFieldProps}
+            inputRef={register(postalCodeValidation)}
           />
-          <br />
-          <br />
           <Button
             variant='contained'
             color='primary'
